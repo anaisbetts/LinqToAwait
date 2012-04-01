e1193fb4519bfea8ddd015774e4f88ccf5f40133
Paul Betts <paul@paulbetts.org>
2012-03-31 18:01:02 -0700

    Initial Import

---
+++ LinqToAwait/Operators.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Reactive.Linq.ToAwait
+{
+    public static class LinqToAwait
+    {
+        public static IObservable<TRet> SelectAsync<T, TRet>(this IObservable<T> This, Func<T, Task<TRet>> selector, bool preserveOrdering = false)
+        {
+            return preserveOrdering ?
+                This.Select(x => selector(x).ToObservable()).Concat() :
+                This.SelectMany(x => selector(x).ToObservable());
+        }
+
+        public static IObservable<T> WhereAsync<T>(this IObservable<T> This, Func<T, Task<bool>> filter)
+        {
+            return This.Select(x => {
+                return filter(x).ToObservable()
+                    .SelectMany(cond => cond ? Observable.Return(x) : Observable.Empty<T>());
+            }).Concat();
+        }
+
+        public static IObservable<bool> AllAsync<T>(this IObservable<T> This, Func<T, Task<bool>> filter)
+        {
+            return This
+                .SelectMany(x => filter(x).ToObservable())
+                .All(x => x != false);
+        }
+
+        public static IObservable<bool> AnyAsync<T>(this IObservable<T> This, Func<T, Task<bool>> filter)
+        {
+            return This
+                .SelectMany(x => filter(x).ToObservable())
+                .Any(x => x != false);
+        }
+
+        /*
+        public static IObservable<T> AsAsync<T>(this IEnumerable<T> This)
+        {
+            return This.ToObservable(TaskScheduler.Current);
+        }
+        */
+    }
+}
