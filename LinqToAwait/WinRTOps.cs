0a58aed7a8dac0dfb00a3a256365b4ef9d4fb6a6
Paul Betts <paul@paulbetts.org>
2012-04-01 21:11:24 -0700

    Add some WinRT goodness

---
+++ LinqToAwait/WinRTOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Windows.Foundation;
+using Windows.Foundation;
+
+namespace LinqToAwait
+{
+    public static class LinqToAwaitWinRT
+    {
+        public static IObservable<TRet> SelectAsync<T, TRet>(this IObservable<T> This, Func<T, IAsyncOperation<TRet>> selector, bool preserveOrdering = false)
+        {
+            return preserveOrdering ?
+                This.Select(x => selector(x).ToObservable()).Concat() :
+                This.SelectMany(x => selector(x).ToObservable());
+        }
+    }
+}
