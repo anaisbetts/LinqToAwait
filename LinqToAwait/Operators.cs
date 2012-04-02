4fae298b7b905607cec922b7c9b4837b739e4ac9
Paul Betts <paul@paulbetts.org>
2012-04-01 22:06:37 -0700

    Namespaces

--- LinqToAwait/Operators.cs
+++ LinqToAwait/Operators.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace System.Reactive.Linq.ToAwait
+namespace LinqToAwait
 {
     public static class LinqToAwait
     {
